3f9714ecaf8c0230de739a0750ff83ef55308607
Campos5 <ecampm02@estudiantes.unileon.es>
2019-02-27 20:32:08 +0100

    :sparkles: pequeños cambios en las rutas de la parte no guiada

--- pr2/noGuiada_4.2.1/Debug/8led.d
+++ pr2/noGuiada_4.2.1/Debug/8led.d
@@ -1,10 +1,10 @@
 8led.d 8led.o: ../8led.c \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44b.h \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/option.h \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44blib.h
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44b.h \
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/option.h \
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44blib.h
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44b.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44b.h:
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/option.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/option.h:
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44blib.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44blib.h:
--- pr2/noGuiada_4.2.1/Debug/main.d
+++ pr2/noGuiada_4.2.1/Debug/main.d
@@ -1,10 +1,10 @@
 main.d main.o: ../main.c \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44blib.h \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44b.h \
- C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/option.h
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44blib.h \
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44b.h \
+ C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/option.h
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44blib.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44blib.h:
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/44b.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/44b.h:
 
-C:\hlocal\EclipseARMwspace\Guiada\commonEclipse/option.h:
+C:\hlocal\sed\SED\pr2\noGuiada_4.2.1\commonEclipse/option.h:
