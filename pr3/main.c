6875ab335bc19d6fdaafa54b342008b692875e2c
Campos5 <ecampm02@estudiantes.unileon.es>
2019-03-17 20:42:09 +0100

    :pencil: añadida estructura para parte 2 de la practica 3. Falta añadir las direcciones y los datos para que las escriba el keyboard y las lea pulsando el boton derecho

--- pr3/main.c
+++ pr3/main.c
@@ -4,12 +4,18 @@
 #include "golden_at24c04.h"
 
 
-extern void DelayMs(int ms_time);
+
 extern void sys_init();
 extern void iic_init();
+extern void Eint4567_init();
+extern void init_buttons();
+extern void keyboard_init();
 extern void D8Led_symbol(int value);
 extern void at24c04_bytewrite( uint16 addr, uint8 data );
 extern void at24c04_byteread( uint16 addr, uint8 *data );
+
+uint16 dir;
+uint8 data;
 
 
 int val;
@@ -18,13 +24,15 @@ void Main( void )
 {
     uint8 buffer[AT24C04_DEPTH];
     uint16 i;
-    
+
     sys_init();
     iic_init();
+    Eint4567_init();
+    keyboard_init();
     D8Led_symbol(8);//8
 
     //Inicializamos la EEPROM
-    for( i=0; i<AT24C04_DEPTH; i++ ){
+   /* for( i=0; i<AT24C04_DEPTH; i++ ){
     	val = (uint8)i%16;
     	at24c04_bytewrite( i, val );
     }
@@ -40,7 +48,7 @@ void Main( void )
     	val = buffer[i] & 0xF;
     	D8Led_symbol(val);
     	DelayMs(100);
-    }
+    }*/
 
     while( 1 );
 }
